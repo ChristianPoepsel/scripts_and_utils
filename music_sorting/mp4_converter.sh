91be4d5a47108dcd60b03c825bae3933354c56ce
ChristianPoepsel <christian.poepsel@gmail.com>
2024-02-17 21:46:04 +0100

    all files now working

--- music_sorting/mp4_converter.sh
+++ music_sorting/mp4_converter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FLAC_DIR="/Volumes/VideoWork1/Musikbibliothek JP FLAC backup/AC DC"
+FLAC_DIR="/Volumes/LR RAW DATA/Music FLAC/AC DC"
 MP4_DIR="/Volumes/LR RAW DATA/Music MP4"
 LOG_FILE="mp4_converter_log.log"
 BITRATE="320k"
@@ -17,7 +17,8 @@ find "$FLAC_DIR" -type f -name "*.flac" | while read -r flac_file; do
         mkdir -p "$(dirname "$mp4_file")"
 
         # Error handling
-        if "$XLD_PATH" -f mp4 -o "$(dirname "$mp4_file")" -t aac -b "$BITRATE" -s "$SAMPLE_RATE" "$flac_file"; then
+        # if "$XLD_PATH" -f mp4 -o "$(dirname "$mp4_file")" -t aac -b "$BITRATE" -s "$SAMPLE_RATE" "$flac_file"; then
+        if ffmpeg -nostdin -i "$flac_file" -c:a aac -b:a "$BITRATE" -vn -f mp4 "$mp4_file"; then
             echo "$(date): $flac_file converted" >> "$LOG_FILE"
         else
             echo "$(date): ERROR converting $flac_file" >> "$LOG_FILE"
